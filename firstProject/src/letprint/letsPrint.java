a306029d464aae0d5058dd97b40ce2d83d31230d
Jancy <itsjayb@yahoo.com>
2023-09-19 13:04:25 -0500

    Third Commit

---
+++ firstProject/src/letprint/letsPrint.java
@@ -0,0 +1,14 @@
+package letprint;
+
+public class letsPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lets do this \t correct");
+		System.out.println("Lets do this \t incorrect");
+		System.out.println("Lets do this \t wrong");
+		System.out.println("Lets do this \t right");
+		System.out.println("Lets do this right \nLets do this wrong");
+	}
+
+}
