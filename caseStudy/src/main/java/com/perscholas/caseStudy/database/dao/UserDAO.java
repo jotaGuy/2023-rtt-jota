36b5c2c5ebdbbdca5abd9f2c0f808e287592bc8d
Jancy <itsjayb@yahoo.com>
2023-12-10 23:40:10 -0600

    class changes

--- caseStudy/src/main/java/com/perscholas/caseStudy/database/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.perscholas.caseStudy.database.dao;
-
-import com.perscholas.caseStudy.enity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends JpaRepository<Users, Long> {
-    public Users findByEmailIgnoreCase(String email);
-}
